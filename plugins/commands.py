da192e84bfd76e59eca0a2223abb533308b49873
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-10-05 13:02:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -96,7 +96,7 @@ async def start(bot, cmd):
                 [
                     [
                         InlineKeyboardButton("🤴 ʙᴏᴛ ᴏᴡɴᴇʀ 🤴", url="https://t.me/{BOT_ONR}"),
-                        InlineKeyboardButton("🍁 ʙᴏᴛ ɢʀᴏᴜᴘ 🍁", url="https://t.me/{MAIN_GROUP}")
+                        InlineKeyboardButton("🍁 ʙᴏᴛ ɢʀᴏᴜᴘ 🍁", url="https://t.me/{MAIN_GROUP}"),
                         InlineKeyboardButton("👀 ᴅᴇᴠ 👀", url="https://t.me/NxtStark")
                     ],
                     [
